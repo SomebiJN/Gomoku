18a8d0e44424e9706312594249739bd1a1f49bf3
Somebi Nwogu <sjnwogu@mun.ca>
2019-03-09 19:07:08 -0330

    Update MainFunctionCall.java

--- MainFunctionCall.java
+++ MainFunctionCall.java
@@ -1,2 +1,35 @@
+import java.util.Scanner;
+
 public class MainFunctionCall {
+
+    private boolean playerISBlack;
+
+    public Gomoku gomoku;
+
+    // Use to process text input from the user.
+    private Scanner scanner = new Scanner(System.in);
+
+    public MainFunctionCall(){
+        /**
+         * the 'next' method of the Scanner and 'matches' of the String
+         * are used to process user input
+         */
+        System.out.println("Do you want a black or white stone?(Y/N)");
+        String IsBlack = (scanner.next());
+
+        System.out.println("Two player mode?(Y/N)");
+        String two_player = (scanner.next());
+
+        if(two_player == "Y")
+        {
+            if(IsBlack == "Y")
+            {
+                Gomoku(true,true);
+            }
+            else
+            {
+                Gomoku(false,true);
+            }
+        }
+    }
 }
